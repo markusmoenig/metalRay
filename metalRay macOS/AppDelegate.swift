5ea08d1869a87aa864ee47c6d4fdc8bd65d31c5c
Markus Moenig <markus@moenig.io>
2023-08-21 11:18:05 +0700

    Initial Commit

---
+++ metalRay macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  metalRay macOS
+//
+//  Created by Markus Moenig on 21/8/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
